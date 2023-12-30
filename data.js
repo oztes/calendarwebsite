a48f145580fdd3cbb774f468688199d8890f13c5
oztes <139756413+oztes@users.noreply.github.com>
2023-12-30 14:40:46 -0500

    newdata

---
+++ data.js
@@ -0,0 +1,14 @@
+const DataModule = (function() {
+    // Function to initialize and fetch data from Google Sheets
+    function fetchSheetData() {
+        // Code to interact with Google Sheets API
+        // Fetch the data and return it in a desired format (e.g., JSON)
+    }
+
+    // Publicly accessible function
+    return {
+        getSheetData: function() {
+            return fetchSheetData();
+        }
+    };
+})();
